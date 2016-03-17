f9f98440783fa3d5bc48119621069093a3a4e869
Haakon Winther <haakonwinther@gmail.com>
2016-03-17 16:29:00 +0100

    Re-organized lib and client
    Need to move some files from Client to lib to make Useraccounts to work
    properly.
    Continuing to clean up code in branch

---
+++ Biogrid0.1/lib/components/home/home.jsx
@@ -0,0 +1,14 @@
+Home = React.createClass({
+  render() {
+    return (
+      <div className="container">
+        <div className="text-center">
+          <h1 id="test">BioGrid Coretex</h1>
+          <p>
+            Internet is getting physical
+          </p>
+        </div>
+      </div>
+    );
+  }
+});
---
+++ Biogrid0.1/lib/components/home/nav.jsx
@@ -0,0 +1,50 @@
+Nav = React.createClass({
+  user() {
+    return Meteor.user();
+  },
+
+  navLinks() {
+    if (!Meteor.loggingIn() && Meteor.user()) {
+      return (
+        <ul className="nav navbar-nav pull-right">
+          <li><a href="#profile">{this.user().emails[0].address}</a></li>
+          <li><a href="/logout">Logout</a></li>
+        </ul>
+      );
+
+    } else {
+      return (
+        <ul className="nav navbar-nav pull-right">
+          <li><a href="/sign-in">Sign In</a></li>
+          <li><a href="/sign-up">Sign Up</a></li>
+        </ul>
+      );
+    }
+  },
+
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-static-top" role="navigation">
+        <div className="container">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+
+            </div>
+            <div className="collapse navbar-collapse">
+              <ul className="nav navbar-nav">
+                <li><a href="/">Home</a></li>
+              </ul>
+              {this.navLinks()}
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+});
