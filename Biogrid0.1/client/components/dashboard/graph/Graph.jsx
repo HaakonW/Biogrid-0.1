e11953084144eed72760fcc04bdd49a9a9725f90
Oscar Eckbo <oscar@eckbo.net>
2016-03-17 11:17:22 +0100

    Split the rendering and calculations in to functions.
    Enhanced the algorithm and split it into functions.
    Added isEmpty, isObjectMoreThanOneDay, writeGraphStringSeveralDays,
    writeGraphStringOneDay.
    removed createGraph from componentDidMount.
    Updated componentDidUpdate to only draw graph after data is loaded.

--- Biogrid0.1/client/components/dashboard/graph/Graph.jsx
+++ Biogrid0.1/client/components/dashboard/graph/Graph.jsx
@@ -3,100 +3,130 @@ Graph = React.createClass({
   /* FIXME How not to render when theres new data in getMeteorData in FrameGraphCo2
   When return true the graph is displayed
   */
-  shouldComponentUpdate:function(nextProps, nextState){
+  shouldComponentUpdate:function(nextProps, nextState) {
     return true;
   },
 
-  //Loops through this.props.sensors and creates a graph in the div with id=gr
-  _createGraph:function(){
-    let allValues = this.props.sensors;
-    let graphString ="";
-    let type;
-    //let day = allValues.day;
+  //check if object is empty
+  isEmpty:function(obj) {
+    let hasOwnProperty = Object.prototype.hasOwnProperty;
 
-    //more than one day
-    if(Object.keys(allValues).length > 1)
-    { //if data contains more than one day
-      type = allValues[0].type;
-      for(let i = 0; i < allValues.length; i++){
-        let dayDate = allValues[i].day;
-        for(let key in allValues[i]){
-          if(key === "values"){
-            for(let j in allValues[i][key]){ // i = hours
-              let hour = j;
-                for(let k in allValues[i][key][j]){ // j = minutes
-                  let minute = k;
-                  for(let h in allValues[i][key][j][k]){ // k = seconds
-                    let second = h;
-                    let value = allValues[i][key][j][k][h];
-                    graphString += dayDate + " " + hour + ":" + minute + ":" + second + "," + value + "\n";
-                    break; //Every minute
-                  }
-                }
+    if (obj == null) return true;
+    if (obj.length > 0) return false;
+    if (obj.length === 0) return true;
+    for(var key in obj) {
+      if (hasOwnProperty.call(obj, key)) return false;
+    }
+    return true;
+  },
+
+  //check if object has more days than one
+  isObjectMoreThanOneDay:function(obj) {
+      return Object.keys(obj).length > 1
+  },
+
+  writeGraphStringSeveralDays:function(allValues){
+    //console.log("severalDays", allValues);
+    let graphString = "";
+    let sensorType;
+    //TODO type = ?
+    for (let day = 0; day < allValues.length; day++) {
+      let dayDate = allValues[day].day;
+      for (let key in allValues[day]) {
+        if (key === "values") {
+          for (let hour in allValues[day][key]) {
+            for (let minute in allValues[day][key][hour]) {
+              for (let second in allValues[day][key][hour][minute]) {
+                let value = allValues[day][key][hour][minute][second];
+                graphString += dayDate + " " + hour + ":" + minute + ":" + second + "," + value + "\n";
+                break; //only 1 post pr minute, not every second
+              }
             }
           }
         }
       }
-    }//end if
-    else if(Object.keys(allValues).length === 1)
-    {
-      allValues = allValues[0];
-      type = allValues.type;
-      let day = allValues.day;
+    }
+    return graphString;
+  },
 
-      for(let key in allValues){
-        if(key === "values"){
-          for(let i in allValues[key]){ // i = hours
-            let hour = i;
-              for(let j in allValues[key][i]){ // j = minutes
-                let minute = j;
-                for(let k in allValues[key][i][j]){ // k = seconds
-                  let second = k;
-                  let value = allValues[key][i][j][k];
-                  graphString += day + " " + hour + ":" + minute + ":" + second + "," + value + "\n";
-                  break;
-                }
-              }
+  writeGraphStringOneDay:function(allValues){
+    //console.log("oneDay", allValues);
+    let graphString = "";
+    let sensorType;
+    //TODO type = ?
+    let dayDate = allValues.day;
+
+    for (let key in allValues) {
+      if (key === "values") {
+        for (let hour in allValues[key]) {
+          for (let minute in allValues[key][hour]) {
+            for (let second in allValues[key][hour][minute]) {
+              let value = allValues[key][hour][minute][second];
+              graphString += dayDate + " " + hour + ":" + minute + ":" + second + "," + value + "\n";
+              break; //only 1 post pr minute, not every second
+            }
           }
         }
-
       }
     }
+    return graphString;
+  },
 
+  createGraph:function(){
+    let sensorData = this.props.sensors; //TODO this.props.readings
+    //console.log("createGraph", sensorData);
+    let graphString;
+    if (this.isEmpty(sensorData)) {
+      graphString = ",\n0,0";
+      this.g = new Dygraph(
+        this.graf, graphString,
+        {
+          title:'No data available',
+          pointSize: 0,
+          highlightCircleSize: 0,
+          showLabelsOnHighlight:false
+        }
+      );
+    }
+    else {
+      graphString = this.isObjectMoreThanOneDay(sensorData) ? this.writeGraphStringSeveralDays(sensorData) : this.writeGraphStringOneDay(sensorData);
 
       this.g2 = new Dygraph(
-      /*document.getElementById('graf')*/
-      this.graf,
-      "Date,Value\n" + graphString,
-      {
-        ylabel: 'Value (' + type + ")",
-        axisLabelColor:'#fff',
-        //xlabel: allValues.sensor_id,
-        title: allValues.type,
-        strokeWidth: 2,
-        //gridLineColor: 'rgb(204,0,255)',
-        color: 'rgb(255,152,43)',
-        fillGraph: true,
-        axisLineColor: 'rgb(255,152,43)',
-        showRangeSelector: true,
-        rangeSelectorHeight: 30,
-        rangeSelectorPlotFillColor: 'rgb(255,152,43)',
-        rangeSelectorPlotStrokeColor: 'rgb(255,152,43)',
-      }
-    );
+        /*document.getElementById('graf')*/
+        this.graf,
+        "Date,Value\n" + graphString,
+        {
+          ylabel: 'Value (type N/A)', // TODO fix this to get the sensors.graph.graphType
+          axisLabelColor:'#fff',
+          //xlabel: allValues.sensor_id,
+          title: sensorData.type, //TODO fix this to get the sensors.sensorType
+          strokeWidth: 2,
+          //gridLineColor: 'rgb(204,0,255)',
+          color: 'rgb(255,152,43)', //TODO get colors based on graphType
+          fillGraph: true,
+          axisLineColor: 'rgb(255,152,43)', //TODO get colors based on graphType
+          showRangeSelector: true,
+          rangeSelectorHeight: 30,
+          rangeSelectorPlotFillColor: 'rgb(255,152,43)', //TODO get colors based on graphType
+          rangeSelectorPlotStrokeColor: 'rgb(255,152,43)', //TODO get colors based on graphType
+        }
+      );
+    }
   },
 
   componentDidMount:function(){
-    this._createGraph();
-    console.log("after _createGraph");
+    //this.createGraph();
+    //console.log("didMount");
   },
 
   componentDidUpdate:function(){
-    this._createGraph();
+    if (this.props.ready) this.createGraph(); //waits until all data is ready to draw the graph
+    //console.log("didUpdate");
   },
 
   render(){
     return(
+      //TODO if data
       <div className="panel panel-default">
         <div className="panel-heading">
           <h3 className="panel-title text-center">tittel</h3>
