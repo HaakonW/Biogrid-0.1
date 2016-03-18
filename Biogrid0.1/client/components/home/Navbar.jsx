32692a62627dd134126e85299b492bc88c453e46
petnwil <peter.n.wilhelmsen@gmail.com>
2016-03-18 17:55:37 +0100

    Minor bugfix
    Still bug. Methods can not be used in render().

--- Biogrid0.1/client/components/home/Navbar.jsx
+++ Biogrid0.1/client/components/home/Navbar.jsx
@@ -1,8 +1,10 @@
 NavBar = React.createClass({
 
+    /*FIXME navlinks can not be a method used in render(). Make it a variable
+    in render.*/
     navLinks() {
       if (!Meteor.loggingIn() && Meteor.user()) {
-        FlowRouter.go("/Dashboard");
+        FlowRouter.go("/site");
         return (
           <ul className="nav navbar-nav pull-right">
             <li><a href="#profile">{Meteor.user().emails[0].address}</a></li>
@@ -21,28 +23,30 @@ NavBar = React.createClass({
 
     render() {
       return (
-        <nav className="navbar navbar-default navbar-static-top" role="navigation">
-          <div className="container">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse">
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                </button>
-                <span className="navbar-brand">
-                </span>
-              </div>
-              <div className="collapse navbar-collapse">
-                <ul className="nav navbar-nav">
-                  <li><a href="/">Biogrid</a></li>
-                </ul>
-                {this.navLinks()}
+        <div>
+          <nav className="navbar navbar-default navbar-static-top" role="navigation">
+            <div className="container">
+              <div className="container-fluid">
+                <div className="navbar-header">
+                  <button type="button" className="navbar-toggle" data-toggle="collapse">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                  <span className="navbar-brand">
+                  </span>
+                </div>
+                <div className="collapse navbar-collapse">
+                  <ul className="nav navbar-nav">
+                    <li><a href="/">Biogrid</a></li>
+                  </ul>
+                  {this.navLinks()}
+                </div>
               </div>
             </div>
-          </div>
-        </nav>
-      );
+          </nav>
+        </div>
+      )
     }
 });
