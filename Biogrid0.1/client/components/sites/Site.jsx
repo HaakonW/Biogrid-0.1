4542e19b54b0ccc79af0629acb16e516bf5187cc
petnwil <peter.n.wilhelmsen@gmail.com>
2016-03-18 17:55:00 +0100

    Added href for sitelinks
    Sitelinks for routing to dashboard with correct siteId

--- Biogrid0.1/client/components/sites/Site.jsx
+++ Biogrid0.1/client/components/sites/Site.jsx
@@ -15,20 +15,30 @@ Site = React.createClass({
     let sitesList;
     if( this.data.ready) {
           sitesList = this.data.sites.map(function(site){
+            let id = site._id;
+            let link = "/dashboard/"+id;
           return(
             <div key={site._id}>
-              <a href="/dashboard"> {site.name} </a>
+              <a href={link}> {site.name} </a>
             </div>
           )
         })
     }
 
     //Navbar is replaced by Nav. Nav in lib/components/home/nav
-    return(
-      <div className="text-center">
-        <h2>Your sites</h2>
-          {sitesList}
-      </div>
-    )
+      if(true) {
+        return(
+          <div className="text-center">
+            <h2>Your sites</h2>
+              {sitesList}
+          </div>
+        )
+      } else {
+        return(
+          <div>
+            dashboard
+          </div>
+        )
+      }
   }
 })
