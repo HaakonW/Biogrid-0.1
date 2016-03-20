e659db242c9900be84c3ea0fab1e885d11b54215
petnwil <peter.n.wilhelmsen@gmail.com>
2016-03-20 16:32:56 +0100

    Bug fixes Content.jsx
    Wasn’t allowed to render child-components inside render. Moved the
    content from userStatus() inside render and made a variable which can
    be used in render.

--- Biogrid0.1/client/components/home/Content.jsx
+++ Biogrid0.1/client/components/home/Content.jsx
@@ -1,32 +1,27 @@
 Content = React.createClass({
-userStatus(){
-  if (!Meteor.user()) {
-    return (
-    <h3 className="text-center">Please log in to access site.</h3>
-    );
-  }
-  else {
-    return (
-      <h3 className="text-center">Logged in</h3>
-    )
-    //FlowRouter.route('/Dashboard')
-  }
-},
 
   render(){
-      return(
-        <div>
-          <div className="row">
-            <div className="col-md-12 col-sm4 ">
-              <div className="jumbotron">
-                <h1 className="text-center">Biogrid Cortex</h1>
-                  <div className="text-center">
-                    {this.userStatus()}
-                  </div>
-              </div>
+
+    let status;
+    if(!Meteor.user()) {
+      status = <h3 className="text-center"> Please log in to access site </h3>
+    } else {
+      status = <h3 className="text-center">Logged in</h3>
+    }
+
+    return(
+      <div>
+        <div className="row">
+          <div className="col-md-12 col-sm4 ">
+            <div className="jumbotron">
+              <h1 className="text-center">Biogrid Cortex</h1>
+                <div className="text-center">
+                  {status}
+                </div>
             </div>
           </div>
         </div>
-      )
+      </div>
+    )
   }
 })
