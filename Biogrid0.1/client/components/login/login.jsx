e6b6afe9047a5e9e0a93a99ca257b0312e143f30
Haakon Winther <haakonwinther@gmail.com>
2016-03-25 14:46:38 +0100

    Login with fault handling

--- Biogrid0.1/client/components/login/login.jsx
+++ Biogrid0.1/client/components/login/login.jsx
@@ -8,20 +8,22 @@ Login = React.createClass({
     return data;
   },
 
+  getInitialState: function(){
+    return {value: ""}
+  },
+
   handleSubmit(e){
     e.preventDefault();
     let email = ReactDOM.findDOMNode(this.refs.email).value.trim();
     let password = ReactDOM.findDOMNode(this.refs.password).value.trim();
-    let user = Meteor.loginWithPassword(email, password, function(err){
+    Meteor.loginWithPassword(email, password, (err) => {
         if(err){
-          console.log(err.reason);
-        }
+          this.setState({value: err.reason});
+         }
         else {
-          console.log("bruker logget inn");
           FlowRouter.go('/site');
         }
     });
-    //FlowRouter.go("/site");
   },
 
   render(){
@@ -30,24 +32,29 @@ Login = React.createClass({
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h3 className="text-center">Please sign in</h3>
+              <h3 className="text-center">login to access your things</h3>
             </div>
-
             <div className="row row-centered">
               <div className="col-md-4 col-centered">
-                <form onSubmit={this.handleSubmit} >
-                  <div className="input-group">
-                    <span className="input-group-addon"><i className="fa fa-user"></i></span>
-                      <input type="text" ref="email" placeholder="Email address" id="email" className="form-control"/>
-                    </div>
-                      <div className="input-group">
-                        <span className="input-group-addon"><i className="fa fa-lock"></i></span>
-                        <input type="password" ref="password" placeholder="Password" className="form-control"/>
+                <form onSubmit={this.handleSubmit}>
+                  <div className="spacer10">
+                    <div className="input-group">
+                      <span className="input-group-addon"><i className="fa fa-user"></i></span>
+                        <input type="text" ref="email" placeholder="Email address" id="email" className="form-control"/>
                       </div>
-                  <button type="submit" className="btn btn-primary form-control">Login</button>
+                  </div>
+                  <div className="spacer10">
+                    <div className="input-group">
+                      <span className="input-group-addon"><i className="fa fa-lock"></i></span>
+                      <input type="password" ref="password" placeholder="Password" className="form-control"/>
+                    </div>
+                  </div>
+
+                  <button type="submit" className="btn btn-primary form-control greenBtn">Login</button>
                 </form>
-                <div ref="responefield">
-                  <p>Responsefield</p>
+                <div>
+                  <br/>
+                    <p className="text-center greenBtn">{this.state.value}</p>
                 </div>
               </div>
             </div>
---
+++ Biogrid0.1/client/components/navbar.jsx
@@ -0,0 +1,51 @@
+Navbar = React.createClass({
+    render() {
+      let userinfo;
+      if(!Meteor.loggingIn() && Meteor.user()) {
+          userinfo = <ul className="nav navbar-nav pull-right">
+            <li>
+              <a className="navCortex" href="/site"> {Meteor.user().emails[0].address}
+                <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+              </a>
+            </li>
+            <li>
+              <a className="navCortex" href="/logout">Logout <span className="glyphicon glyphicon-off" aria-hidden="true">
+                </span>
+              </a>
+            </li>
+          </ul>;
+      }
+      else{
+        userinfo = <ul className="nav navbar-nav pull-right">
+
+          <li>
+            <a className="navCortex" href="http:www.biogrid.no">Biogrid.no <span className="glyphicon glyphicon-home" aria-hidden="true">
+              </span>
+            </a>
+          </li>
+        </ul>;
+
+      }
+
+      return (
+        <div>
+          <nav className="navbar navbar-default navbar-static-top" role="navigation">
+            <div className="container">
+              <div className="container-fluid">
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav pull-left"><li>
+                        <a className="navCortex" href="/"><span className="glyphicon glyphicon-grain" aria-hidden="true"> </span> Biogrid Cortex
+                        </a>
+                      </li>
+                    </ul>
+                </div>
+                <div className="collapse navbar-collapse">
+                    {userinfo}
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      )
+    }
+});
