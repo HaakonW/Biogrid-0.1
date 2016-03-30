11a2d573c7abd3146338da92d8172ebe58cab82c
Haakon Winther <haakonwinther@gmail.com>
2016-03-30 17:33:18 +0200

    Testing the Google Maps Api

--- TestKoding/graph/graph.js
+++ TestKoding/graph/graph.js
@@ -1,69 +1,10 @@
-MyCollection = new Meteor.Collection('sensors');
-//retur = MyCollection.find({ day: '2016-02-12', type: 'rh'}, {"values.13.47":1});
-retur = MyCollection.find({ day: '2016-02-12', type: 'rh'}).fetch();
-<<<<<<< HEAD
-//console.log(retur);
+if (Meteor.isClient) { Meteor.startup(function() { GoogleMaps.load();}); } 
 
+let map;
 
-//console.log("HEI "+JSON.stringify(retur));
-
-
-if (Meteor.isClient) {
-
-
-=======
-
-if (Meteor.isClient) {
->>>>>>> origin/master
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-  var startTime = new Date().getMilliseconds();
-<<<<<<< HEAD
-    //console.log(retur[0]);
-    var allValues = retur[0];
-
-=======
-  var graphString = "";
-    //console.log(retur[0]);
-    var allValues = retur[0];
-    var day = allValues.day;
->>>>>>> origin/master
-    for(var key in allValues){
-      if(key == "values")
-      for(var i in allValues[key]){ // i = hours
-        var hour = i;
-          //console.log(i);
-          for(var j in allValues[key][i]){ // j = minutes
-            var minute = j;
-            //console.log(j);
-<<<<<<< HEAD
-            for(var k in allValues[key][i][j]){
-              var second = k;
-              var value = allValues[key][i][j][k];
-              //console.log("hour:" + hour + "| minute: " + minute + "| second: " + second + "| value: " + value);
-=======
-            for(var k in allValues[key][i][j]){ // k = seconds
-              var second = k;
-              var value = allValues[key][i][j][k];
-              //console.log("hour:" + hour + "| minute: " + minute + "| second: " + second + "| value: " + value);
-              graphString += day + "," + hour + "-" + minute + "-" + second + "," + value + "\n";
->>>>>>> origin/master
-            }
-          }
-      }
-    }
-    var endTime = new Date().getMilliseconds();
-    var usedTime = endTime + startTime;
-<<<<<<< HEAD
-    console.log("Algorithm takes " + usedTime + " ms." );
-    //console.log(allValues);
-
-
-=======
-    //console.log(graphString);
-    console.log("Algorithm takes " + usedTime + " ms." );
->>>>>>> origin/master
-   });
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
 }
