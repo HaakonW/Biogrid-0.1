8cfc7b08b73f15231a536d5ad48bcd98387ed543
Oscar Eckbo <oscar@eckbo.net>
2016-04-05 15:30:31 +0200

    Added functionality to subnavbar
    FIXME: Bug on reloading of page.
    TODO: Get correct dates in today, month and week, they are currently
    hardcoded.

--- Biogrid0.1/client/components/graph/parentGraph.jsx
+++ Biogrid0.1/client/components/graph/parentGraph.jsx
@@ -7,8 +7,28 @@ ParentGraph = React.createClass({
     const subscriptionReadings = Meteor.subscribe('readings', this.props.sensorId);
     const subscriptionThings = Meteor.subscribe('things', this.props.thingId);
 
+    let timePeriod = Session.get('timePeriod');
+
     if(subscriptionReadings.ready()) {
-      data.readings = Readings.find({sensorId:this.props.sensorId}).fetch();
+      switch (timePeriod) {
+        case 'week':
+          data.readings = Readings.find({sensorId:this.props.sensorId, //from 7 days ago
+            day: {
+              $gte: "2016-29-03",
+              $lt: "2016-05-04"
+            }}).fetch();
+          break;
+          case 'month':
+          data.readings = Readings.find({sensorId:this.props.sensorId, //from 30 days ago
+            day: {
+              $gte: "2016-02-08",
+              $lt: "2016-05-04"
+            }}).fetch();
+          break;
+        default:
+        data.readings = Readings.find({sensorId:this.props.sensorId, day: "2016-02-09"}).fetch(); //today
+      }
+      //data.readings = Readings.find({sensorId:this.props.sensorId}).fetch();
     }
     if(subscriptionThings.ready()) {
       let thing = Things.find({_id:this.props.thingId}).fetch();
--- Biogrid0.1/client/components/navbar/subNavbar.jsx
+++ Biogrid0.1/client/components/navbar/subNavbar.jsx
@@ -1,4 +1,22 @@
 SubNavbar = React.createClass({
+  handleButtonClicks(period) {
+    //timePeriod = new ReactiveVar();
+    switch (period) {
+      case 'today':
+        Session.set('timePeriod', 'today');
+        break;
+      case 'week':
+        Session.set('timePeriod', 'week');
+        break;
+      case 'month':
+        Session.set('timePeriod', 'month');
+        break;
+      default:
+        //Session.set('timePeriod', 'today');
+    }
+    console.log(Session.get('timePeriod'));
+  },
+
   render(){
     return(
       <nav className="navbar navbar-inverse navbar-static-top" id="subNavbar" role="navigation">
@@ -22,9 +40,9 @@ SubNavbar = React.createClass({
                   </li>
               </ul>
               <ul className="nav navbar-nav">
-                <li className="subNavbarLi"><a href="">Day</a></li>
-                <li className="subNavbarLi"><a href="">Week</a></li>
-                <li className="subNavbarLi"><a href="">Month</a></li>
+                <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'today')}>Today</a></li>
+                <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'week')}>Week</a></li>
+                <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'month')}>Month</a></li>
               </ul>
             </div>
       </nav>
