5aef52996dccbc4c5c37f1f8ab5b7f7047e3ab23
Haakon Winther <haakonwinther@gmail.com>
2016-04-06 10:23:39 +0200

    SubNavbar with dropdown list
    Dropdown list with your sites now inside the subnavbar.

--- Biogrid0.1/client/components/navbar/subNavbar.jsx
+++ Biogrid0.1/client/components/navbar/subNavbar.jsx
@@ -1,52 +1,65 @@
 SubNavbar = React.createClass({
+  mixins:[ReactMeteorData],
+  getMeteorData() {
+    let data = {};
+    data.sites = [];
+    const subscription = Meteor.subscribe('sites');
+    return {
+      ready: subscription.ready(),
+      sites: Sites.find().fetch(),
+    }
+  },
   handleButtonClicks(period) {
     //timePeriod = new ReactiveVar();
     switch (period) {
       case 'today':
-        Session.set('timePeriod', 'today');
-        break;
+      Session.set('timePeriod', 'today');
+      break;
       case 'week':
-        Session.set('timePeriod', 'week');
-        break;
+      Session.set('timePeriod', 'week');
+      break;
       case 'month':
-        Session.set('timePeriod', 'month');
-        break;
+      Session.set('timePeriod', 'month');
+      break;
       default:
-        //Session.set('timePeriod', 'today');
+      //Session.set('timePeriod', 'today');
     }
     console.log(Session.get('timePeriod'));
   },
-
   render(){
+    let sitesList;
+    if( this.data.ready) {
+      sitesList = this.data.sites.map(function(site){
+        let id = site._id;
+        let link = "/dashboard/"+id;
+        return(
+          <div key={site._id}>
+            <li><a className="queryLinks" href={link}> {site.name} </a></li>
+          </div>
+        )
+      })
+    } else {
+      sitesList = <li> Sorry, no sites available or loading </li>
+    }
     return(
       <nav className="navbar navbar-inverse navbar-static-top" id="subNavbar" role="navigation">
-            <div className="navbar-header collapse navbar-collapse">
-              <ul className="nav navbar-nav">
-                  <li className="dropdown">
-                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                      Current Site <i className="fa fa-caret-down greenIcon"></i> </a>
-                    <ul className="dropdown-menu">
-                      <li><a href="#">Time Period:</a></li>
-                      <li role="separator" className="divider"></li>
-                      <li><a href="#">Day</a></li>
-                      <li><a href="#">Week</a></li>
-                      <li><a href="#">Month</a></li>
-                      <li role="separator" className="divider"></li>
-                      <li className="subNavbarLi"><a href="#">Custom data</a></li>
-                      <li role="separator" className="divider"></li>
-                      <li><a href="#">From</a></li>
-                      <li><a href="#">To</a></li>
-                    </ul>
-                  </li>
+        <div className="navbar-header collapse navbar-collapse">
+          <ul className="nav navbar-nav">
+            <li className="dropdown subNavbarLi">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                Change Site <i className="fa fa-caret-down greenIcon"></i> </a>
+              <ul className="dropdown-menu" id="yourSites">
+                {sitesList}
               </ul>
-              <ul className="nav navbar-nav">
-                <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'today')}>Today</a></li>
-                <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'week')}>Week</a></li>
-                <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'month')}>Month</a></li>
-              </ul>
-            </div>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav">
+            <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'today')}>Today</a></li>
+            <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'week')}>Week</a></li>
+            <li className="subNavbarLi"><a href="" onClick={this.handleButtonClicks.bind(this, 'month')}>Month</a></li>
+          </ul>
+        </div>
       </nav>
-
     )
   }
 });
