067594b63b72ab117967babf3c4e44f4f0cb8c8f
petnwil <peter.n.wilhelmsen@gmail.com>
2016-04-11 19:24:04 +0200

    Removed unused publish
    Removed publish from Sensors-collection which is not in use anymore.

--- Biogrid0.1/lib/collections/_collections.js
+++ Biogrid0.1/lib/collections/_collections.js
@@ -1,4 +1,3 @@
-
 Sites = new Mongo.Collection('sites');
 Hubs = new Mongo.Collection('hubs');
 Firmwares = new Mongo.Collection('firmware');
@@ -7,14 +6,6 @@ Things = new Mongo.Collection('things');
 Readings = new Mongo.Collection('readings');
 
 if(Meteor.isServer){
-  /*Sensors = new Mongo.Collection('sensors');
-  Meteor.publish('sensors',function(){
-    return Sensors.find({day:"2016-03-10"});
-    /*return Sensors.find({day:{
-                          $gte: "2016-02-19",
-                          $lt: "2016-03-11"
-                        }});
-  });*/
 
   //TODO pass userID as argument in function
   Meteor.publish('sites', function(){
