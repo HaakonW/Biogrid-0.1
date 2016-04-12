9ecea376f48301a46fcacc62d2b1d96a26138191
Haakon Winther <haakonwinther@gmail.com>
2016-04-12 12:50:33 +0200

    Nicer setup for the metadata component

--- Biogrid0.1/client/components/dashboard/metaData.jsx
+++ Biogrid0.1/client/components/dashboard/metaData.jsx
@@ -1,29 +1,34 @@
 MetaData = React.createClass({
   render(){
     return(
-      <div className="container">
         <div className="row" id="metaData">
-          <div className="col-md-12">
-            <div className="col-md-2 col-xs-5 metaCol" id="firstMeta">
-              <h1 className="countNumber">12</h1>
-              <p className="metaText pull-right">Sensors</p>
+          <div className="col-md-12" >
+            <div className="col-md-2 col-xs-5 col-md-offset-2 metaCol" >
+              <div className="col-md-12" id="firstMeta">
+                <h1 className="countNumber">12</h1>
+                <p className="metaText pull-right">Sensors</p>
+              </div>
             </div>
-            <div className="col-md-2 col-xs-5 metaCol" id="secondMeta">
-              <h1 className="countNumber">5</h1>
+            <div className="col-md-2 col-xs-5 metaCol">
+              <div className="col-md-12" id="secondMeta">
+                <h1 className="countNumber">5</h1>
                 <p className="metaText pull-right">Things</p>
+              </div>
             </div>
-            <div className="col-md-2 col-xs-5 metaCol" id="thirdMeta">
-              <h1 className="countNumber">{this.props.countHubs}</h1>
+            <div className="col-md-2 col-xs-5 metaCol">
+              <div className="col-md-12" id="thirdMeta">
+                <h1 className="countNumber">{this.props.countHubs}</h1>
                 <p className="metaText pull-right">Hubs</p>
+              </div>
             </div>
-            <div className="col-md-2 col-xs-5 metaCol" id="forthMeta">
-              <h1 className="countNumber">0</h1>
+            <div className="col-md-2 col-xs-5 metaCol">
+              <div className="col-md-12" id="forthMeta">
+                <h1 className="countNumber">0</h1>
                 <p className="metaText pull-right">Unconnected</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
     )
   }
 })
